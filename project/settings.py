0ffbe6d84d592cb6b4fe365166f4317b61011a77
Kimberley Tham <kimbertham@gmail.com>
2020-10-23 18:43:07 +0100

    stateic

--- project/settings.py
+++ project/settings.py
@@ -131,6 +131,18 @@ USE_L10N = False
 USE_TZ = False
 
 # DATETIME_FORMAT="%Y-%m-%d%H:%M:%S"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 REST_FRAMEWORK = {  # added this to get rest framework to use our custom authentication classes
     'DEFAULT_RENDERER_CLASSES': [
