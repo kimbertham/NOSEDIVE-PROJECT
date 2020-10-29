4527667397c720c0339aa79d779968a26cdf9efc
Kimberley Tham <kimbertham@gmail.com>
2020-10-29 18:57:56 +0000

    d

--- posts/models.py
+++ posts/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     image= models.CharField(max_length=10000, blank=True)
     profile_owner = models.ForeignKey('jwt_auth.User',
     related_name='page_posts', 
-    on_delete=models.CASCADE)
+    on_delete=models.CASCADE, blank=True)
 
     owner = models.ForeignKey(
     'jwt_auth.User',
